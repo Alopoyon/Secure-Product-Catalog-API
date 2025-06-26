4c37c7adeef8170320233ff9b6f9ac204015ddaf
AchMat <43774510+Alopoyon@users.noreply.github.com>
2025-06-26 23:37:03 +0530

    orders model

---
+++ app/models/orders.py
@@ -0,0 +1,20 @@
+from models import metadata_obj
+from sqlalchemy import Table, Column, VARCHAR, Integer, ForeignKeyConstraint
+
+from uuid import uuid4
+
+orders = Table(
+    "orders",
+    metadata_obj,
+    Column("id", VARCHAR(36), nullable=False, default=uuid4),
+    Column("product_id", Integer, nullable=False),
+    # Column("user_id", VARCHAR(36), nullable=False),
+
+    ForeignKeyConstraint(
+        ["product_id"],
+        ["products.id"],
+        name="fk_product_id",
+        ondelete="CASCADE",
+        onupdate="CASCADE"
+    )
+)
