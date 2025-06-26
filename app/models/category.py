6bea2e43bf342198c5a9de2592f4e91841e4f853
AchMat <43774510+Alopoyon@users.noreply.github.com>
2025-06-26 22:23:42 +0530

    products and categories models

--- app/models/category.py
+++ app/models/category.py
@@ -1,10 +1,17 @@
 from models import metadata_obj
-from sqlalchemy import Table, Column, String, Integer
+from sqlalchemy import Table, Column, String, Integer, ForeignKeyConstraint
 
 categories = Table(
     "categories",
     metadata_obj,
-    Column("category_id", Integer, primary_key=True),
-    Column("category_name", String(50)),
-    Column("description", String(200))
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50)),
+    Column("parent_id", Integer),
+    Column("description", String(200)),
+
+    ForeignKeyConstraint(["parent_id"], ["categories.id"],
+                         name="fk_sub_categories",
+                         ondelete="CASCADE",
+                         onupdate="CASCADE"
+                         )
 )
--- app/models/products.py
+++ app/models/products.py
@@ -1,17 +1,21 @@
 from models import metadata_obj
-from sqlalchemy import Table, Column, Integer, String, Boolean, Computed, ForeignKeyConstraint
+from sqlalchemy import Table, Column, Integer, String, Boolean, Computed, ForeignKeyConstraint, Float
 
 
 product = Table(
     "product",
     metadata_obj,
-    Column("product_id", Integer, primary_key=True),
-    Column("product_name", String(150)),
-    Column("stock", Integer), 
-    Column("active", Boolean, Computed("stock > 0")),
-    Column("category"),
+    Column("id", Integer, primary_key=True),
+    Column("name", String(150)),
+    Column("category", Integer),
+    Column("stock", Integer),
+    Column("price", Float),
+    Column("active", Boolean, Computed("stock > 0 || price <0")),
 
-    ForeignKeyConstraint(["category"], ["categories.category_id"], name="fk_category", ondelete="CASCADE")
-
+    ForeignKeyConstraint(["category"], ["categories.id"], 
+                         name="fk_category", 
+                         ondelete="CASCADE", 
+                         onupdate="CASCADE"
+                         )
 
 )
