46734a02e3e1b21b4870235ab31a220db0bbea87
AchMat <43774510+Alopoyon@users.noreply.github.com>
2025-07-21 17:05:42 +0530

    removed routes dir to reduce clutter

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from fastapi.routing import APIRouter
-from app.api.v1.routes import category, order, product
+from app.api.v1 import category, order, product
 
 router = APIRouter()
 
 router.include_router(product.router)
 router.include_router(category.router)
+router.include_router(order.router)
 
 __all__ = ["router"]
